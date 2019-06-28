6972e1bd057be0abeede75c9eeee8ee1286f905b
Marissa <marissa.shieh@gmail.com>
2019-06-28 14:44:44 -0700

    Swap blank PhotoGallery into index, replacing Part 1's App

---
+++ src/components/PhotoGallery.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class PhotoGallery extends React.Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+    return (
+      <div>
+        Test
+      </div>
+    )
+  }
+}
+
+export default PhotoGallery;
