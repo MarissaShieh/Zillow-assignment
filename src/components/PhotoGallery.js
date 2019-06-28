1d9263de020576dfa2e96bec02c9534f553665d1
Marissa <marissa.shieh@gmail.com>
2019-06-28 16:21:58 -0700

    Test rendering single image from sample data

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import samplePhotos from '../data/samplePhotoData';
 
 class PhotoGallery extends React.Component {
   constructor(){
     super();
+    this.state = {
+      loading: true,
+      photos: [],
+      currentPhotoIndex: null
+    }
+  }
+
+  componentDidMount() {
+    // Get photos to display.
+    // Can be refactored to display photos retrieved from database
+
+    const photos = samplePhotos;
+    console.log(photos[0].url.masterBedroom);
+    this.setState({
+      loading: false,
+      photos,
+      currentPhotoIndex: Math.floor(photos.length/2), // default first photo is middle photo
+    });
   }
 
   render(){
     return (
       <div>
-        Test
+        {this.state.loading? <div>Loading...</div> : <img src={this.state.photos[0].url} alt={this.state.photos[0]}/>} 
       </div>
     )
   }
