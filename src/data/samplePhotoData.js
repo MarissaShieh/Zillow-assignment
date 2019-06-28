b10c2c44b2d3d94e411048a27fab41d8bb7cd7fd
Marissa <marissa.shieh@gmail.com>
2019-06-28 16:35:33 -0700

    Add more photos to the sample data

--- src/data/samplePhotoData.js
+++ src/data/samplePhotoData.js
@@ -1,10 +1,31 @@
-import masterBedroom from '../assets/ialicante-mediterranean-homes-bed1-unsplash.jpg';
+import masterBedroom from '../assets/ialicante-mediterranean-homes-master-unsplash.jpg';
+import front from '../assets/ialicante-mediterranean-homes-front-unsplash.jpg';
+import kitchen from '../assets/ialicante-mediterranean-homes-kitchen-unsplash.jpg';
+import juniorBedroom from '../assets/ialicante-mediterranean-homes-bed1-unsplash.jpg';
+import stairs from '../assets/ialicante-mediterranean-homes-stairs-unsplash.jpg';
 
-
-const data = [{
-  url: `${masterBedroom}`,
-  caption: 'placeholder text'
-}];
+const data = [
+  {
+    url: `${front}`,
+    caption: 'Front of the home'
+  },
+  {
+    url: `${masterBedroom}`,
+    caption: 'Master bedroom of the home'
+  },
+  {
+    url: `${kitchen}`,
+    caption: 'The kitchen at the center of the home'
+  },
+  {
+    url: `${stairs}`,
+    caption: 'Stairs between floors'
+  },
+  {
+    url: `${juniorBedroom}`,
+    caption: 'The smaller of the two bedrooms'
+  }
+];
 
 
 export default data;
